7e2b532ecf920896bb49d6e544f1b4d71dcdf44d
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-03-29 03:46:44 +0330

    khaste shodam

--- core/core/settings.py
+++ core/core/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'website' 
+    'website',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -158,3 +159,8 @@ if SHOW_DEBUGGER_TOOLBAR:
     import socket  # only if you haven't already imported this
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
+
+
+
+AUTH_USER_MODEL = 'accounts.User'
+
