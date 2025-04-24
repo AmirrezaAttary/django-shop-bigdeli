a9edd76e45f3ee51174ef487ede5808c55b540c2
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-04-24 22:35:37 +0330

    feat: enhance cart functionality with product quantity update and removal features; update related views and templates

--- core/cart/urls.py
+++ core/cart/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = "cart"
 
 urlpatterns = [
-    path('session/add-product/',views.SessionAddProduct.as_view(),name='session-add-product'),
-    path('session/cart/summary/',views.SessionCartSummry.as_view(),name='session-cart-summery'),
+    path('session/add-product/',views.SessionAddProductView.as_view(),name='session-add-product'),
+    path('session/cart/summary/',views.SessionCartSummryView.as_view(),name='session-cart-summary'),
+    path("session/update-product-quantity/",views.SessionUpdateProductQuantityView.as_view(),name="session-update-product-quantity"),
+    path("session/remove-product/",views.SessionRemoveProductView.as_view(),name="session-remove-product"),
 ]
 
