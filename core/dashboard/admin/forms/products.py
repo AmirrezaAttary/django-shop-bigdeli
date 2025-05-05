470a6472c36c6e01517e40c24ebe16decaf84445
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-05-04 23:54:48 -0700

    Refactor code structure for improved readability and maintainability

--- core/dashboard/admin/forms/products.py
+++ core/dashboard/admin/forms/products.py
@@ -1,5 +1,5 @@
 from django import forms
-from shop.models import ProductModel
+from shop.models import ProductModel, ProductImageModel
 from django_ckeditor_5.widgets import CKEditor5Widget
 
 class ProductForm(forms.ModelForm):
@@ -34,3 +34,18 @@ class ProductForm(forms.ModelForm):
         self.fields['status'].widget.attrs['class'] = 'form-select'
         self.fields['price'].widget.attrs['class'] = 'form-control'
         self.fields['discount_percent'].widget.attrs['class'] = 'form-control'
+        
+        
+class ProductImageForm(forms.ModelForm):
+
+
+    class Meta:
+        model = ProductImageModel
+        fields = [
+            "file",
+        ]
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['file'].widget.attrs['class'] = 'form-control'
+        self.fields['file'].widget.attrs['accept'] = 'image/png, image/jpg, image/jpeg'
