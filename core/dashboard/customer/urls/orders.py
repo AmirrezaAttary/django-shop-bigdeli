1a73e7b7041eea3f5f3c45160e105cd9e32e9c39
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-05-10 10:02:23 +0330

    feat: Implement admin order management with list, detail, and invoice views, including filtering and pagination

--- core/dashboard/customer/urls/orders.py
+++ core/dashboard/customer/urls/orders.py
@@ -4,4 +4,5 @@ from .. import views
 urlpatterns = [
     path("order/list/",views.CustomerOrderListView.as_view(),name="order-list"),
     path("order/<int:pk>/detail/",views.CustomerOrderDetailView.as_view(),name="order-detail"),
+    path("order/<int:pk>/invoice/",views.CustomerOrderInvoiceView.as_view(),name="order-invoice"),
 ]
