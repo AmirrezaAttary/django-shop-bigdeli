dc1835dbd24fed1ca254c069302c4ecefad7ca01
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-05-17 00:57:12 +0330

    feat: Enhance cart functionality with stock validation and update methods; add stock reduction after successful payment

--- core/payment/views.py
+++ core/payment/views.py
@@ -5,7 +5,8 @@ from django.urls import reverse_lazy
 from django.shortcuts import redirect, get_object_or_404
 from .zarinpal_client import ZarinPalSandbox
 from order.models import OrderModel, OrderStatusType
-
+from django.db import transaction
+from shop.models import ProductModel
 # Create your views here.
 
     
@@ -21,14 +22,20 @@ class PaymentVerifyView(View):
         response = zarin_pal.payment_verify(int(payment_obj.amount),payment_obj.authority_id)
         print(response)
         if status == 'OK':
-            payment_obj.ref_id = response['data']["ref_id"]
-            payment_obj.response_code = response['data']["code"]
-            payment_obj.status = PayemntStatusType.success.value
-            payment_obj.response_json=response
-            payment_obj.save()
-            order.status = OrderStatusType.success.value
-            order.save()
+            with transaction.atomic():
+                payment_obj.ref_id = response['data']["ref_id"]
+                payment_obj.response_code = response['data']["code"]
+                payment_obj.status = PayemntStatusType.success.value
+                payment_obj.response_json = response
+                payment_obj.save()
+
+                order.status = OrderStatusType.success.value
+                order.save()
+
+                self.reduce_stock(order)
+
             return redirect(reverse_lazy("order:completed"))
+
         else:
             # payment_obj.ref_id = response['data']["ref_id"]
             payment_obj.response_code = response['errors']["code"]
@@ -38,3 +45,14 @@ class PaymentVerifyView(View):
             order.status = OrderStatusType.failed.value
             order.save()
             return redirect(reverse_lazy("order:failed"))
+        
+        
+    def reduce_stock(self, order):
+        """کاهش موجودی محصولات سفارش داده شده پس از پرداخت موفق"""
+        for item in order.order_items.all():
+            product = item.product
+            if product.stock >= item.quantity:
+                product.stock -= item.quantity
+                product.save()
+            else:
+                raise ValueError(f"موجودی محصول '{product.title}' کافی نیست.")
