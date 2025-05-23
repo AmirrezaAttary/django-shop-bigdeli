1fb09b613fba155c259968bc2b177aef58f534df
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2025-05-23 21:57:19 +0330

    fix bug

--- core/shop/views.py
+++ core/shop/views.py
@@ -23,38 +23,35 @@ class ShopProductListView(ListView):
     
     
 class ShopProductGridView(ListView):
-    template_name = 'shop/product-grid.html'
+    template_name = "shop/product-grid.html"
     paginate_by = 9
 
-
     def get_paginate_by(self, queryset):
-        return self.request.GET.get('page_size',self.paginate_by)
-    
+        return self.request.GET.get('page_size', self.paginate_by)
+
     def get_queryset(self):
-        queryset = ProductModel.objects.filter(status=ProductStatusType.publish.value)
-        search_q=self.request.GET.get('q')
-        if search_q:
+        queryset = ProductModel.objects.filter(
+            status=ProductStatusType.publish.value)
+        if search_q := self.request.GET.get("q"):
             queryset = queryset.filter(title__icontains=search_q)
-        category_id=self.request.GET.get('category_id')
-        if category_id:
+        if category_id := self.request.GET.get("category_id"):
             queryset = queryset.filter(category__id=category_id)
-
-        if min_price:= self.request.GET.get('min_price'):
+        if min_price := self.request.GET.get("min_price"):
             queryset = queryset.filter(price__gte=min_price)
-
-        if max_price:= self.request.GET.get('max_price'):
+        if max_price := self.request.GET.get("max_price"):
             queryset = queryset.filter(price__lte=max_price)
-
         if order_by := self.request.GET.get("order_by"):
             try:
                 queryset = queryset.order_by(order_by)
             except FieldError:
                 pass
         return queryset
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['total_items'] = self.get_queryset().count()
-        context["wishlist_items"] = WishlistProductModel.objects.filter(user=self.request.user).values_list("product__id",flat=True)
+        context["total_items"] = self.get_queryset().count()
+        context["wishlist_items"] = WishlistProductModel.objects.filter(user=self.request.user).values_list(
+            "product__id", flat=True) if self.request.user.is_authenticated else []
         context["categories"] = ProductCategoryModel.objects.all()
         return context
     
@@ -78,7 +75,8 @@ class ShopProductDetailView(DetailView):
 
         context['in_cart'] = quantity > 1 or any(item['product_id'] == str(product.id) for item in cart_item.get('items', []))
         context['quantity'] = quantity
-        context["is_wished"] = WishlistProductModel.objects.filter(user=self.request.user,product__id=self.get_object().id).exists()
+        context["is_wished"] = context["is_wished"] = WishlistProductModel.objects.filter(
+            user=self.request.user, product__id=self.get_object().id).exists() if self.request.user.is_authenticated else False
         return context
     
 
@@ -109,18 +107,20 @@ class ShopProductAddOneQuantityView(View):
     
     
     
-class AddOrRemoveWishlistView(LoginRequiredMixin,View):
-    
-    def post(self,request,*args,**kwargs):
+class AddOrRemoveWishlistView(LoginRequiredMixin, View):
+
+    def post(self, request, *args, **kwargs):
         product_id = request.POST.get("product_id")
         message = ""
         if product_id:
             try:
-                wishlist_item =WishlistProductModel.objects.get(user=request.user,product__id=product_id)
+                wishlist_item = WishlistProductModel.objects.get(
+                    user=request.user, product__id=product_id)
                 wishlist_item.delete()
-                message="محصول از لیست علایق حذف شد"
+                message = "محصول از لیست علایق حذف شد"
             except WishlistProductModel.DoesNotExist:
-                WishlistProductModel.objects.create(user=request.user,product_id=product_id)
-                message="محصول به لیست علایق اضافه شد"
-        
-        return JsonResponse({"message":message})
+                WishlistProductModel.objects.create(
+                    user=request.user, product_id=product_id)
+                message = "محصول به لیست علایق اضافه شد"
+
+        return JsonResponse({"message": message})
